b8b79f81e4483a1b8542826db0aa38c91b4b8ff6
Anil Sarioglu <s109345@ap.be>
2020-04-23 14:35:56 +0200

    Created basic model, repo and controller

--- src/main/java/edu/ap/facilitytoolspringboot/Models/Defect.java
+++ src/main/java/edu/ap/facilitytoolspringboot/Models/Defect.java
@@ -1,4 +1,48 @@
 package edu.ap.facilitytoolspringboot.Models;
 
+import javax.persistence.*;
+
+@Entity
 public class Defect {
+    @Id
+    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(nullable = false)
+    private String title;
+
+    private int numberUpvote;
+
+    public Defect() {
+    }
+
+    public Defect(String title) {
+        this.title = title;
+        this.numberUpvote = 2;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumberUpvote() {
+        return numberUpvote;
+    }
+
+    @Override
+    public String toString() {
+        return "Defect{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", numberUpvote=" + numberUpvote +
+                '}';
+    }
 }
--- src/main/java/edu/ap/facilitytoolspringboot/Repositories/DefectRepository.java
+++ src/main/java/edu/ap/facilitytoolspringboot/Repositories/DefectRepository.java
@@ -1,4 +1,7 @@
 package edu.ap.facilitytoolspringboot.Repositories;
 
-public interface DefectRepository {
+import edu.ap.facilitytoolspringboot.Models.Defect;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DefectRepository extends JpaRepository<Defect, Long> {
 }
